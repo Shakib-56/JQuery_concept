780f11be5596571c5b08dad79aeef72136118972
Shakib <hossenmdshakib47@gmail.com>
2026-01-02 04:21:01 +0600

    basic about jQuery and learn upto before and after method

---
+++ index.js
@@ -0,0 +1,20 @@
+// document.querySelector("h1").innerHTML = "Hello, welcome to JQuery journey!";
+// here class selectror is used to change the text of h1
+// $(".my-div h1").text("hi");
+// here id selector is used to change the html of paragraph one and it's style
+// $("#p1").html("<b>This is paragraph one.</b>");
+
+// some text append to the previous paragraph text
+// $("#p1").append(" This text is appended to the previous paragraph text.");
+
+// some text prepend to the previous paragraph text
+// $("#p1").prepend(" This text is prepended to the previous paragraph text.");
+
+// create a new paragraph using JQuery and add before previous paragraph
+// let mypara1 = $("<p></p>").text(
+//   "This is a new paragraph created using JQuery."
+// );
+// $("#p1").before(mypara1.clone());
+// // create a new paragraph using JQuery and add after previous paragraph
+
+// $("#p1").after(mypara1.clone());
